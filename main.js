20a0257e0c62fe5fdfc7b0b68c66c7d87110f75e
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2022-04-25 21:16:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+fetch('https://jessicagilfillan.github.io/weirdDeals1/products.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+function appendData(data) {
+    var mainContainer = document.querySelector("main");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = 'Name: ' + data[i].name + ' ' + data[i].price;
+        mainContainer.appendChild(div);
+    }
+}
